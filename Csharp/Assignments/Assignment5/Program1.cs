f57eba7227cd22cad63708ebeecd62ae43432739
Nishitha Galla <nishithag@infinite.com>
2025-07-03 23:45:52 +0530

    Assignment5 added

---
+++ Csharp/Assignments/Assignment5/Program1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    
+    class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException(string message): base(message) { }
+    }
+
+    class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
+
+        public BankAccount(string holder, double balance)
+        {
+            AccountHolder = holder;
+            Balance = balance;
+        }
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Deposit amount must be more than 0");
+
+            Balance += amount;
+            Console.WriteLine($"Deposited {amount}/-. Current balance: {Balance}/-");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Withdraw amount must be positive");
+          
+            if (amount > Balance)
+                throw new InsufficientBalanceException("No enough balance");
+
+            Balance -= amount;
+            Console.WriteLine($"Withdrawn {amount}/-, Remaining balance: {Balance}/-");
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Current Balance: {Balance}/-");
+        }
+    } 
+    class Program1
+    {
+        static void Main()
+        {
+            BankAccount acc = new BankAccount("Nishitha", 2000);
+
+            try
+            {
+                Console.WriteLine($"Welcome, {acc.AccountHolder}");
+                Console.WriteLine("Enter transaction type D for Deposit, W for Withdraw: ");
+                string type = Console.ReadLine();
+
+                Console.Write("Enter amount: ");
+                double amount = Convert.ToDouble(Console.ReadLine());
+
+                if (type == "D" || type == "d")
+                    acc.Deposit(amount);
+                else if (type == "W" || type == "w")
+                    acc.Withdraw(amount);
+                else
+                    Console.WriteLine("Invalid transaction type");
+
+                acc.ShowBalance();
+                
+            }
+            catch (InsufficientBalanceException ex)
+            {
+                Console.WriteLine("Custom exception: " + ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Csharp/Assignments/Assignment5/Program2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    
+    class NotEligibleException : Exception
+    {
+        public NotEligibleException(string message) : base(message) { }
+    }
+    class Scholarship
+    {
+        public void Merit(double marks, double fees)
+        {
+            double scholarshipAmount;
+
+            if (marks >= 70 && marks <= 80)
+                scholarshipAmount = 0.20 * fees;
+            else if (marks > 80 && marks <= 90)
+                scholarshipAmount = 0.30 * fees;
+            else if (marks > 90)
+                scholarshipAmount = 0.50 * fees;
+            else
+                throw new NotEligibleException("Sorry, you are not eligible for scholarship. Your marks are below 70");
+
+            Console.WriteLine($"Scholarship amount: {scholarshipAmount}/-");
+            Console.WriteLine($"Fees after scholarship: {fees - scholarshipAmount}/-");
+        }
+    }
+    class Program2
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter marks: ");
+                double marks = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter total fees: ");
+                double fees = Convert.ToDouble(Console.ReadLine());
+
+                Scholarship s = new Scholarship();
+                s.Merit(marks, fees);
+            }
+            catch (NotEligibleException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input, please enter numbers only...");
+            }
+            Console.Read();
+        }
+    }
+}
