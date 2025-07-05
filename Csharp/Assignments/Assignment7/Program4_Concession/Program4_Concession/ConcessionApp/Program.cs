00a451266143f61d86725cddd7ca22b5d41e22a2
Nishitha Galla <nishithag@infinite.com>
2025-07-06 03:06:35 +0530

    Assignment7 added

---
+++ Csharp/Assignments/Assignment7/Program4_Concession/Program4_Concession/ConcessionApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConcessionLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcessionApp
+{
+    class Program
+    {
+
+        const double TotalFare = 500; 
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            TravelConcession tc = new TravelConcession();
+            tc.CalculateConcession(name, age, TotalFare);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp/Assignments/Assignment7/Program4_Concession/Program4_Concession/ConcessionLib/TravelConcession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcessionLib
+{
+    public class TravelConcession
+    {
+        public void CalculateConcession(string name, int age, double totalFare)
+        {
+            if (age <= 5)
+            {
+                Console.WriteLine("Little Champs - Free Ticket");
+            }
+            else if (age > 60)
+            {
+                double concession = totalFare * 0.30;
+                double finalAmount = totalFare - concession;
+                Console.WriteLine($"Senior citizen - {name}, finalAmount: {finalAmount}/-");
+            }
+            else
+            {
+                Console.WriteLine($"Ticket booked to {name} for {totalFare}/-");
+            }
+        }
+    }
+}
